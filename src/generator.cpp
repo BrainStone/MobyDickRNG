fe1740d6851f8377401772a5720ad4f61ef4ba1b
BrainStone <yannick@tedworld.de>
2018-01-23 00:52:09 +0100

    Implemented generator

---
+++ src/generator.cpp
@@ -0,0 +1,41 @@
+//#define GOLFED
+
+#ifdef GOLFED
+
+// TODO!
+
+#else // GOLFED
+
+#include "generator.hpp"
+
+void generator::initDistribution( const std::string& text ) {
+	typedef std::map<std::string::value_type, uint64_t> distributionMap;
+
+	distributionMap letterDistribution;
+
+	for ( std::string::const_reference letter : text ) {
+		++letterDistribution[letter];
+	}
+
+	generator::letters = std::vector<std::string::value_type>();
+	std::vector<uint64_t> counts = std::vector<uint64_t>();
+
+	generator::letters.reserve( letterDistribution.size() );
+	counts.reserve( letterDistribution.size() );
+
+	for ( distributionMap::const_reference count : letterDistribution ) {
+		generator::letters.push_back( count.first );
+		counts.push_back( count.second );
+	}
+
+	generator::distr = std::discrete_distribution<size_t>( counts.cbegin(), counts.cend() );
+}
+
+generator::generator( std::minstd_rand::result_type seed ) :
+	rng( seed ) {}
+
+std::string::value_type generator::getNextChar() const {
+	return letters.at( distr( rng ) );
+}
+
+#endif // !GOLFED
---
+++ src/generator.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+class generator {
+private:
+	static std::discrete_distribution<size_t> distr;
+	static std::vector<std::string::value_type> letters;
+
+	const std::minstd_rand rng;
+
+public:
+	static void initDistribution( const std::string& text );
+
+	generator( std::minstd_rand::result_type seed );
+
+	std::string::value_type getNextChar() const;
+};
