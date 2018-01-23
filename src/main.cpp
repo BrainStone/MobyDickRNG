9069e75982c21ba3eb88f89d690a030f051ebef2
BrainStone <yannick@tedworld.de>
2018-01-23 01:20:47 +0100

    Implemented basic search

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.hpp"
 
 int main() {
-	;
+	generator::initDistribution( mobyDick );
+
+	for ( std::minstd_rand::result_type i = 0; i < 10; ++i ) {
+		generator gen( i );
+
+		std::cout << "i = " << i << " -> " << calculateScore( [&gen]() -> uint8_t {return gen.getNextChar(); } ) << std::endl;
+	}
 }
