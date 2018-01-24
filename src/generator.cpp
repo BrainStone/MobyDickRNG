fba22bae41dc3c86de93b4f5d972eafa170f600b
BrainStone <yannick@tedworld.de>
2018-01-24 11:11:57 +0100

    Added discrete_mapped_distribution
    - A class that takes a map of distributions as inputs

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,30 +1,15 @@
 #include "generator.hpp"
 
-std::discrete_distribution<size_t> generator::distr;
-std::vector<std::string::value_type> generator::letters;
+discrete_mapped_distribution<std::string::value_type> generator::letters;
 
 void generator::initDistribution(const std::string& text) {
-	typedef std::map<std::string::value_type, uint64_t> distributionMap;
-
-	distributionMap letterDistribution;
+	std::map<std::string::value_type, uint64_t> letterDistribution;
 
 	for (std::string::const_reference letter : text) {
 		++letterDistribution[letter];
 	}
 
-	generator::letters = std::vector<std::string::value_type>();
-	std::vector<uint64_t> counts = std::vector<uint64_t>();
-
-	generator::letters.reserve(letterDistribution.size());
-	counts.reserve(letterDistribution.size());
-
-	for (distributionMap::const_reference count : letterDistribution) {
-		generator::letters.push_back(count.first);
-		counts.push_back(count.second);
-	}
-
-	generator::distr = std::discrete_distribution<size_t>(counts.cbegin(),
-			counts.cend());
+	generator::letters = discrete_mapped_distribution<std::string::value_type>(letterDistribution);
 }
 
 generator::generator(std::minstd_rand::result_type seed) :
@@ -36,7 +21,7 @@ void generator::setSeed(std::minstd_rand::result_type seed) {
 }
 
 std::string::value_type generator::getNextChar() {
-	return letters.at(distr(rng));
+	return letters(rng);
 }
 
 // TODO: Golf code!
--- src/generator.hpp
+++ src/generator.hpp
@@ -3,20 +3,58 @@
 #include <map>
 #include <random>
 #include <string>
+#include <type_traits>
 #include <vector>
+
+template<typename T>
+class discrete_mapped_distribution {
+private:
+	std::discrete_distribution<size_t> distr;
+	std::vector<T> values;
+
+public:
+	discrete_mapped_distribution() :
+		distr(), values() {
+	}
+	template<typename I, typename = typename std::enable_if<
+			std::is_arithmetic<I>::value, I>::type>
+	discrete_mapped_distribution(std::map<T, I> distribution) :
+			values() {
+		std::vector<I> counts = std::vector<I>();
+
+		values.reserve(distribution.size());
+		counts.reserve(distribution.size());
+
+		for (typename std::map<T, I>::const_reference count : distribution) {
+			values.push_back(count.first);
+			counts.push_back(count.second);
+		}
+
+		distr = std::discrete_distribution<size_t>(counts.cbegin(), counts.cend());
+	}
+
+	discrete_mapped_distribution(const discrete_mapped_distribution&) = default;
+	discrete_mapped_distribution& operator=(const discrete_mapped_distribution&) = default;
+
+	template<typename URNG>
+	T operator()(URNG& urng) {
+		return values.at(distr(urng));
+	}
+};
 
 class generator {
 private:
-	static std::discrete_distribution<size_t> distr;
-	static std::vector<std::string::value_type> letters;
+	static discrete_mapped_distribution<std::string::value_type> letters;
 
 	std::minstd_rand rng;
 
 public:
-	static void initDistribution( const std::string& text );
+	static void initDistribution(const std::string& text);
 
-	generator( std::minstd_rand::result_type seed = std::minstd_rand::default_seed );
+	generator(std::minstd_rand::result_type seed =
+			std::minstd_rand::default_seed);
 
-	void setSeed( std::minstd_rand::result_type seed = std::minstd_rand::default_seed );
+	void setSeed(std::minstd_rand::result_type seed =
+			std::minstd_rand::default_seed);
 	std::string::value_type getNextChar();
 };
